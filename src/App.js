365d214a1f5264f64528f669bf162564994276b6
Cassandra1245 <guillermogarciasuarez@gmail.com>
2024-12-02 21:40:36 +0100

    d

--- src/App.js
+++ src/App.js
@@ -4,49 +4,56 @@ import './App.css';
 import Carrito from './Carrito';
 
 function App() {
-
-  const [ListaArticulos, setListaArticulos] = useState([])
-  const [CarritoCompra, setCarritoCompra] = useState([])
-  const [showElement, setShowElement] = useState([false])
+  const [ListaArticulos, setListaArticulos] = useState([]);
+  const [CarritoCompra, setCarritoCompra] = useState([]);
+  const [showElement, setShowElement] = useState(false);
 
   useEffect(() => {
-
     const fetchListaArticulos = async () => {
       const response = await fetch('./articulos_navideños.json');
       const data = await response.json();
       setListaArticulos(data);
     };
     fetchListaArticulos();
-  }, [])
+  }, []);
 
-  function setCarritoVisibilidad() {
-    setShowElement(prevState => !prevState); 
-  }
+  const setCarritoVisibilidad = () => {
+    setShowElement((prevState) => !prevState);
+  };
+
+  const añadirAlCarrito = (articulo) => {
+    if (!CarritoCompra.some((item) => item.id === articulo.id)) {
+      setCarritoCompra([...CarritoCompra, articulo]);
+    } 
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <ul>
-          {ListaArticulos.map(articulo => (
+          {ListaArticulos.map((articulo) => (
             <li key={articulo.id}>
               <h3>{articulo.nombre}</h3>
               <p>{articulo.descripcion}</p>
               <p>{articulo.precio}</p>
-              <button onClick={() => setCarritoCompra([...CarritoCompra, articulo])} >Añadir al carrito</button>
+              <input
+                type="button"
+                value="Añadir al carrito"
+                onClick={() => añadirAlCarrito(articulo)}
+              />
             </li>
           ))}
         </ul>
-        <button onClick={setCarritoVisibilidad}>
-        {showElement ? 'Ocultar carrito' : 'Mostrar carrito'}
-        </button>
-        <div >
-            {showElement && <Carrito carrito={CarritoCompra}/>}
-        </div>
+        <input
+          type="button"
+          value={showElement ? 'Ocultar carrito' : 'Mostrar carrito'}
+          onClick={setCarritoVisibilidad}
+        />
+        <div>{showElement && <Carrito carrito={CarritoCompra} />}</div>
       </header>
     </div>
   );
 }
 
 export default App;
-
--- src/Carrito.js
+++ src/Carrito.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Carrito({ carrito }) {
+
+  const [cantidadTotal, setCantidad] = useState ()
+
+  const establecerCantidad = (cantidad) => {
+      const newCantidad = (cantidad.target.value)
+      setCantidad(newCantidad);
+  };
+
 
   return (
     <div>
       <h2>Carrito de la Compra</h2>
-        <ul>
-          {carrito.map((articulo) => (
-            <li key={articulo.id}>
-              {articulo.nombre}
-            </li>
-          ))}
-        </ul>
+      <ul>
+        {carrito.map((articulo) => (
+          <li key={articulo.id}>
+            <p>{articulo.id} - {articulo.nombre}</p>
+            <input type="number"  step="1" min="0" value={cantidadTotal} onChange={establecerCantidad}/>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
